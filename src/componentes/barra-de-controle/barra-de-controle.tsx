bc51de36f6dc1ed02ede860fce20de9fbbc34714
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-01-03 17:58:28 -0300

    Desenvolvido estrutura botão barra de controle

--- src/componentes/barra-de-controle/barra-de-controle.tsx
+++ src/componentes/barra-de-controle/barra-de-controle.tsx
@@ -1,11 +1,27 @@
+import { Button } from "react-bootstrap";
+import { AiOutlinePlus } from "react-icons/ai";
 import "./barra-de-controle.scss";
 
-export default function ComponenteBarraControle(){
+
+export default function ComponenteBarraControle() {
   return (
     <section id="BarraDeControle">
-      <p>
-        opa
-      </p>
+      <Button className="botao" variant="btn">
+        <div className="containerIcone">
+          <AiOutlinePlus className="icone" />
+        </div>
+        <span className="escrita">
+          Contador Regressivo
+        </span>
+      </Button>
+      <Button className="botao" variant="btn">
+        <div className="containerIcone">
+          <AiOutlinePlus className="icone" />
+        </div>
+        <span className="escrita">
+          Contador Progressivo
+        </span>
+      </Button>
     </section>
   )
 }
